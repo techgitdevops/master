df6a61ee254464a5e83845fa6fdc8ec6a77c3a16
sonu prasad <sonuprasad71229@gmail.com>
2024-11-06 22:02:26 +0530

    Delete aap.py

--- aap.py
+++
@@ -1,9 +0,0 @@
-import boto3
-client = boto3.client('rds')
-response = client.run_instances(
-   ImageId='ami-08ec94f928cf25a9d',
-   InstanceType='t2.micro',
-   KeyName='frankfurtkey',
-   MaxCount=1,
-   MinCount=1
-)
