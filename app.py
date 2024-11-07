040114c84216d1f12500bbca2838b8770f19133a
sonu <sonuprasad71229@gmail.com>
2024-11-07 21:42:58 +0530

    changes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ client = boto3.client('ec2')
 response = client.run_instances(
    ImageId='ami-08ec94f928cf25a9d',
    InstanceType='t2.micro',
-   KeyName='frankfurtkey',
+   KeyName='remote',
    MaxCount=1,
    MinCount=1
 )
