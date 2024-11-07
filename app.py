33cd0481033d8600ad34d6e8d9d7db17acd9ac71
sonu prasad <sonuprasad71229@gmail.com>
2024-11-07 21:04:20 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import boto3
-client = boto3.client('s3')
-response = client.run_instances(
-   ImageId='ami-08ec94f928cf25a9d',
-   InstanceType='t2.micro',
-   KeyName='frankfurtkey',
-   MaxCount=1,
-   MinCount=1
-)
