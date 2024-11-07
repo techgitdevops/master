c5eb3f2f21396a90494edf2f6a6b5ce71022c94e
sonu prasad <sonuprasad71229@gmail.com>
2024-11-07 21:37:10 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import boto3
+client = boto3.client('ec2')
+response = client.run_instances(
+   ImageId='ami-08ec94f928cf25a9d',
+   InstanceType='t2.micro',
+   KeyName='frankfurtkey',
+   MaxCount=1,
+   MinCount=1
+)
